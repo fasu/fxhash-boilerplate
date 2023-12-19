87526855059ec00d0b861c699edf844c404ea15f
Teppo Lindell <teppo.lindell@gofore.com>
2023-12-19 19:14:53 +0200

    random korjattu

--- initial/sketch.js
+++ initial/sketch.js
@@ -6,52 +6,39 @@ const C = {
   prop() {
     return this.height / this.width;
   },
-  isLandscape() {
-    return window.innerHeight <= window.innerWidth * this.prop();
-  },
-  resize() {
-    if (this.isLandscape()) {
-      console.log("yes");
-      document.getElementById(this.css).style.height = "100%";
-      document.getElementById(this.css).style.removeProperty("width");
-    } else {
-      document.getElementById(this.css).style.removeProperty("height");
-      document.getElementById(this.css).style.width = "100%";
-    }
-  },
   setSize(w, h, p, css) {
     (this.width = w), (this.height = h), (this.pD = p), (this.css = css);
   },
   createCanvas() {
-    (this.main = createCanvas(this.width, this.height, WEBGL)),
-      pixelDensity(this.pD),
-      this.main.id(this.css),
-      this.resize();
+    (this.main = createCanvas(this.width, this.height, WEBGL)), pixelDensity(1), this.main.id(this.css);
   },
 };
 C.setSize(1500, 2000, 1, "mainCanvas");
-
-function windowResized() {
-  C.resize();
-}
 
 //////////////////////////////////////////////////
 // The example really starts here
 
 let palette = ["#7b4800", "#002185", "#003c32", "#fcd300", "#ff2702", "#6b9404"];
 
-function preload() {
-  brush.config({
-    R: () => $fx.rand(),
-  });
-}
-
 function setup() {
-  brush.config;
+  console.log(brush.config);
+  const fxseed = $fx.rand() * 10420;
+  randomSeed(fxseed);
+  noiseSeed(fxseed);
   C.createCanvas();
   angleMode(DEGREES);
   background("#fffceb");
+  console.log($fx.rand());
+  //  console.log(fxrand());
+  brush.config({
+    R: () => $fx.rand(), // or any other RNG function you wish to use
+  });
 
+  console.log(brush);
+}
+
+function draw() {
+  console.log(brush.config);
   translate(-width / 2, -height / 2);
 
   // We create a grid here
@@ -73,23 +60,23 @@ function setup() {
   for (let i = 0; i < num_rows; i++) {
     for (let j = 0; j < num_cols; j++) {
       // We fill 10% of the cells
-      if (random() < 0.1) {
+      if ($fx.rand() < 0.1) {
         // Set Fill
-        brush.fill(random(palette), random(60, 100));
-        brush.bleed(random(0.1, 0.4));
+        brush.fill(randomFromArray(palette), randInt(60, 100));
+        brush.bleed(randNumber(0.1, 0.4));
         brush.fillTexture(0.55, 0.8);
       }
 
       // We stroke + hatch the remaining
       else {
         // Set Stroke
-        brush.set(random(stroke_brushes), random(palette));
+        brush.set(randomFromArray(stroke_brushes), randomFromArray(palette));
 
         // Set Hatch
         // You set color and brush with .setHatch(brush_name, color)
-        brush.setHatch(random(hatch_brushes), random(palette));
+        brush.setHatch(randomFromArray(hatch_brushes), randomFromArray(palette));
         // You set hatch params with .hatch(distance_between_lines, angle, options: see reference)
-        brush.hatch(random(10, 60), random(0, 180), { rand: 0, continuous: false, gradient: false });
+        brush.hatch(randInt(10, 60), randInt(0, 180), { rand: 0, continuous: false, gradient: false });
       }
 
       // We draw the rectangular grid here
@@ -101,4 +88,21 @@ function setup() {
       brush.noHatch();
     }
   }
+  noLoop();
+}
+
+function rand() {
+  return $fx.rand();
+}
+
+function randInt(minimi, maksimi) {
+  return Math.floor(minimi + (maksimi + 1 - minimi) * $fx.rand());
+}
+
+function randNumber(minimi, maksimi) {
+  return minimi + (maksimi - minimi) * $fx.rand();
+}
+
+function randomFromArray(array) {
+  return array[randInt(0, array.length - 1)];
 }
